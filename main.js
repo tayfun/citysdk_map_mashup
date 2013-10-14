1b2b922e8429f1c348815cd89556c2789b439a30
Tayfun Sen <tayfun@redbeacon.com>
2013-10-14 04:14:12 +0300

    Full screen map.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 ymaps.ready(function() {
+  if (navigator.geolocation) {
+    $(".loading").show();
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+  }
+  function geoSuccess(position) {
+    console.log("got position from geo api");
+    initMap(position.coords.latitude, position.coords.longitude);
+  }
+  function geoError() {
+    console.log("got position from yandex ip map");
+    initMap(ymaps.geolocation.latitude, ymaps.geolocation.longitude);
+  }
+  function initMap(lat, lng) {
+    $(".loading").remove();
     var myMap = new ymaps.Map('mapcon', {
-    // center: [39.889847, 32.810152],
-    center: [ymaps.geolocation.latitude, ymaps.geolocation.longitude],
-    zoom: 13
-  });
+      center: [lat, lng],
+      zoom: 15
+    });
+  }
 });
