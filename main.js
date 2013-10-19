986e6804232a75ea3d42845f49f412320651604b
Tayfun Sen <tayfun@redbeacon.com>
2013-10-19 22:39:48 +0300

    Add namespace, controls.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Our namespace.
+var topluTasima = {};
+
+// We can start using ymaps on ready.
 ymaps.ready(function() {
   if (navigator.geolocation) {
     $(".loading").show();
@@ -5,19 +9,25 @@ ymaps.ready(function() {
 
   }
   function geoSuccess(position) {
-    console.log("got position from geo api");
+    console.log("got position from geo api", position.coords);
     initMap(position.coords.latitude, position.coords.longitude);
   }
   function geoError() {
-    console.log("got position from yandex ip map");
+    console.log("got position from yandex using ip", ymaps.geolocation);
     initMap(ymaps.geolocation.latitude, ymaps.geolocation.longitude);
   }
   function initMap(lat, lng) {
     $(".loading").remove();
-    var myMap = new ymaps.Map('mapcon', {
+    topluTasima.map = new ymaps.Map('mapcon', {
       center: [lat, lng],
       zoom: 15,
-      behaviors: ["drag", "dblClickZoom", "multiTouch"]
+      // behaviors: ["drag", "dblClickZoom", "multiTouch"]
+      behaviors: ["default"]
     });
+    topluTasima.mapcon = $("#mapcon");
+    if (topluTasima.mapcon.width() > 500) {
+        // For higher resolution devices, add a zoom control.
+        topluTasima.map.controls.add('smallZoomControl');
+    }
   }
 });
